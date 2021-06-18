dc79ab56ae218eb00037f985914820141ac913cc
Denvalkir <denbodrov@yandex.ru>
2021-06-18 16:03:11 +0500

    4.1.2. Глупый бот [#31323 #168844]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -2,13 +2,16 @@ package ru.job4j.condition;
 
 public class DummyBot {
     public static String answersBot(String text) {
+        String answer;
+        
         if (text.equals("Привет, Бот.")) {
-            return "Привет, умник.";
+            answer = "Привет, умник.";
         } else if (text.equals("Пока.")) {
-            return "До скорой встречи.";
+            answer = "До скорой встречи.";
         } else {
-            return "Это ставит меня в тупик. Задайте другой вопрос.";
+            answer = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
+        return answer;
     }
 
     public static void main(String[] args) {
